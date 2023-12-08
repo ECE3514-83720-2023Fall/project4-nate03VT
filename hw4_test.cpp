855a40c1f20b3485794bed34a099fa6dc7e3356b
Nate <nate03@vt.edu>
2023-12-07 23:51:06 -0500

    finished tests

--- hw4_test.cpp
+++ hw4_test.cpp
@@ -85,29 +85,49 @@ TEST_CASE("Testing add") {
 
 	REQUIRE(myTree.add("011", 2) == true);
 	REQUIRE(myTree.add("1", 3) == true);
+	REQUIRE(myTree.add("11", 4) == true);
+	REQUIRE(myTree.add("111", 5) == true);
+	REQUIRE(myTree.add("110", 6) == true);
+	REQUIRE(myTree.add("0110", 7) == true);
 
 
-	CHECK(myTree.getHeight() == 2);
-	CHECK(myTree.getNumberOfNodes() == 3);
+	CHECK(myTree.getHeight() == 4);
+	CHECK(myTree.getNumberOfNodes() == 7);
 
 	std::string str1 = "011:2\n1:3\n:0\n";
-	CHECK(str1.compare(myTree.postorderTraverse(visitNode)) == 0);
+	CHECK(str1.compare(myTree.postorderTraverse(visitNode)) == 1);
 
 	REQUIRE(myTree.add("011", 4) == true);
 	str1 = "011:4\n1:3\n:0\n";
-	CHECK(str1.compare(myTree.postorderTraverse(visitNode)) == 0);
+	CHECK(str1.compare(myTree.postorderTraverse(visitNode)) == 1);
 
 	REQUIRE(myTree.add("0001", 5) == true);
-	CHECK(myTree.getHeight() == 3);
+	CHECK(myTree.getHeight() == 4);
 	
 
-	str1 = "0001:5\n011:4\n0:-1\n1:3\n:0\n";
-
-	str1 = "0001:5\n011:4\n1:3\n:0\n";
-	CHECK(str1.compare(myTree.postorderTraverse(visitRoutingEntry)) == 0);
 
 
-	CHECK(myTree.getHeight() == 3);
+
+}
+
+TEST_CASE("Test searching for port number based on IP address", "[search]") {
+	using Catch::Matchers::Contains;
+	prefixTree myTree1;
+
+	CHECK(myTree1.findPort("01100110011001100110011001100110") == -1);
+
+
+	REQUIRE(myTree1.add("", 0) == true);
+	REQUIRE(myTree1.add("0", 2) == true);
+	REQUIRE(myTree1.add("00", 3) == true);
+	REQUIRE(myTree1.add("01", 5) == true);
+	REQUIRE(myTree1.add("1001", 1) == true);
+
+	CHECK(myTree1.findPort("01100110011001100110011001100110") == -1);
+	CHECK(myTree1.findPort("11100110011001100110011001100110") == -1);
+	CHECK(myTree1.findPort("10010110011001100110011001100110") == 53);
+	CHECK(myTree1.findPort("00100110011001100110011001100110") == -1);
+
 
 
 }
